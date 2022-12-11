5d3223fe3d8f0de5576ed9e4f9c1b686d869df61
Bagas <mbagas221@gmail.com>
2022-12-11 15:50:49 +0700

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,470 @@
+const greeting = {
+  title: "Hello There! 👋.",
+  title2: "Bagas Sudibyo",
+  logo_name: "<bagas/>",
+  nickname: "bagas",
+  full_name: "Muhammad Bagas Sudibyo",
+  subTitle: "Full Stack Developer 🔥",
+  resumeLink:
+    "https://drive.google.com/file/d/1Vhc03EsaxQJaa1tIyTab65jeUj0C9DKK/view",
+  mail: "mailto:mbagas221@gmail.com",
+};
+
+const socialMediaLinks = {
+  github: "https://github.com/bagasdisini",
+  linkedin: "https://www.linkedin.com/in/bagasdisini/",
+  gmail: "mbagas221@gmail.com",
+  instagram: "https://www.instagram.com/mhbagas/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Build scalable and robust APIs and system",
+        "⚡ Build reusable code and libraries for future use",
+        "⚡ Developing mobile applications using React Native",
+        "⚡ Creating application backend in Go & Node.js",
+        "⚡ Integration of third party services",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#563d7c",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "Go",
+          fontAwesomeClassname: "simple-icons:go",
+          style: {
+            color: "#29BEB0",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+  ],
+};
+ 
+const degrees = {
+  degrees: [
+    {
+      title: "Electronic Engineering Polytechnic Institute of Surabaya",
+      subtitle: "Diploma in Informatics Engineering",
+      logo_path: "pens.png",
+      alt_name: "PENS",
+      duration: "2022 - Present",
+      descriptions: [
+        "⚡ I'm currently pursuing my diploma in Informatics Engineering.",
+      ],
+      website_link: "https://www.pens.ac.id/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Basic Visuallisasi Data",
+      subtitle: "Dicoding",
+      logo_path: "dicoding.png",
+      certificate_link:
+        "https://dicoding.com/certificates/81P2G3G8JPOY",
+      alt_name: "Dicoding",
+      color_code: "#2D3E50",
+    },
+    {
+      title: "C2 Proficient English Level",
+      subtitle: "EF Standard English Test",
+      logo_path: "ef.png",
+      certificate_link:
+        "https://www.efset.org/cert/qctqim",
+      alt_name: "EF Standard English Test",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Computer Technical Support",
+      subtitle: "LSP TIK Triatma Kompetensi",
+      logo_path: "tik.jfif",
+      certificate_link:
+        "https://www.efset.org/cert/qctqim",
+      alt_name: "LSP TIK Triatma Kompetensi",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "DS0101EN Data Science 101",
+      subtitle: "Cognitive Class by IBM",
+      logo_path: "ibm.png",
+      certificate_link:
+        "https://courses.cognitiveclass.ai/certificates/abdad1b153684626b20446a144254010",
+      alt_name: "Cognitive Class by IBM",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Data Analyst Professional Academy",
+      subtitle: "Digitalent Kominfo",
+      logo_path: "digitalent.jpg",
+      certificate_link:
+        "https://bucket.cloud.lintasarta.co.id:8082/dts-sertifikat/sertifikat-pdf/32abe38b-b1c0-463e-9f67-b49be4beb443.pdf",
+      alt_name: "Digitalent",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Full Stack Developer Certified",
+      subtitle: "PT DumbWays Teknologi Indonesia",
+      logo_path: "dw.png",
+      certificate_link:
+        "https://dumbways.id/certificate?u=630d196b89017d003737b655",
+      alt_name: "PT DumbWays Teknologi Indonesia",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Healthy And Safe Telework",
+      subtitle: "OpenWHO",
+      logo_path: "who.png",
+      certificate_link:
+        "https://openwho.org/verify/xelis-bisov-moral-gizym-gypar",
+      alt_name: "OpenWHO",
+      color_code: "#0B72B5",
+    },
+    {
+      title: "Memulai Pemrograman Dengan Python",
+      subtitle: "Dicoding",
+      logo_path: "dicoding.png",
+      certificate_link:
+        "https://www.dicoding.com/certificates/07Z6RVM9RPQR",
+      alt_name: "Dicoding",
+      color_code: "#2D3E50",
+    },
+    {
+      title: "The Fundamental of Digital Marketing",
+      subtitle: "Google Digital Garage",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://learndigital.withgoogle.com/link/1hqdd1n3z7k",
+      alt_name: "Google Digital Garage",
+      color_code: "#FFFFFF",
+    },
+  ],
+};
+
+const experience = {
+  title: "Experience",
+  subtitle: "Work Experience",
+  description:
+    "I've so much experience working with teams. I am actively looking for full-time and part-time work. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Product Administrator",
+          company: "PT Dharma Satya Nusantara Tbk",
+          company_url: "https://dsn.co.id/",
+          logo_path: "dsn.jfif",
+          duration: "Apr 2021 - Mei 2022",
+          location: "Muara Wahau, East Borneo",
+          description: `Working with the Manager, manage and update the resource planning, to ensure the product data and information is current and accurate. Support the Manager with monthly product report and monthly cost production for regional trend analysis. Ensure all data entered into the system is accurate and consistent in its format.`,
+          color: "#FFFFFF",
+        },
+        {
+          title: "Freelance Advertiser",
+          company: "ClickDealer",
+          company_url: "https://www.clickdealer.com/",
+          logo_path: "cd.jfif",
+          duration: "Sept 2018 - Jul 2021",
+          location: "Remote",
+          description: `Creates marketing campaigns to promote a variety of products or services. Conducting market research, collaborating with the sales and design teams to create targeted ad campaigns. Monitoring the performance of ad campaigns to measure return on investment and to inform future marketing campaigns.`,
+          color: "#2A8EE8",
+        },
+      ],
+    },
+  ],
+};
+
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create Go Backend Projects, React Projects, and Python Scripts. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "bagas.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "data-analyst-professional-academy",
+      url: "https://github.com/bagasdisini/Data-Analyst-DQLab-x-Kominfo-Professional-Academy",
+      description:
+        "A collection of scripts for Digitalent Data Analyst DQLab x Kominfo Professional Academy.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "ways-food",
+      url: "https://github.com/bagasdisini/integration-react-golang",
+      description:
+        "Website for buying food online, anywhere anytime.",
+      languages: [
+        {
+          name: "React JS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Go",
+          iconifyClass: "logos-go",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "PostgreSQL",
+          iconifyClass: "logos-postgresql",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "ways-to-do-app",
+      url: "https://github.com/bagasdisini/react-native",
+      description:
+        "A mobile app for that generally used to maintain day-to-day tasks or list everything that have to do, with the most latest tasks at the top of the list, and the oldest tasks at the bottom.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "PostgreSQL",
+          iconifyClass: "logos-postgresql",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "ways-gallery",
+      url: "https://github.com/bagasdisini/ways-gallery-fe",
+      description:
+        "A website design creators gather to share their work with other creators",
+      languages: [
+        {
+          name: "React JS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Go",
+          iconifyClass: "logos-go",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "PostgreSQL",
+          iconifyClass: "logos-postgresql",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "golang-url-shortener",
+      url: "https://github.com/bagasdisini/golang-url-shortener",
+      description:
+        "A Simple and small URL shortener project using Go and MySQL.",
+      languages: [
+        {
+          name: "Go",
+          iconifyClass: "logos-go",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "personal-portfolio",
+      url: "https://github.com/bagasdisini/portfolio",
+      description:
+        "A simple website to know more about me :).",
+      languages: [
+        {
+          name: "React JS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
